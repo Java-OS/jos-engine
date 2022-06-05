e0d52de66084fae0fed2b3113c97227d88ae5dcc
mah454 <mah454@gmail.com>
2022-06-05 12:18:51 +0430

    cleanup code

--- jos-shell/src/main/java/module-info.java
+++ jos-shell/src/main/java/module-info.java
@@ -1,5 +1,3 @@
-import ir.moke.jos.module.ModuleContextImpl;
-
 module jos.shell {
     requires text.table.formatter;
 
@@ -8,7 +6,6 @@ module jos.shell {
     requires info.picocli;
     requires org.fusesource.jansi;
     requires jos.common;
-    uses ModuleContextImpl;
     opens ir.moke.jos.shell.command;
     exports ir.moke.jos.shell;
     opens ir.moke.jos.shell.command.module;
